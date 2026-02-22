811a42ad1d14e3cf08fd6e7c16acab99e2aeb24e
marcos <marcos@msegovia.dev>
2026-02-22 12:01:58 +0100

    adding specific movement for jsons

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -7,13 +7,15 @@ return {
   },
   build = ':TSUpdate',
   config = function()
+    local parsers = { "vim", "vimdoc", "lua", "java", "javascript", "typescript", "html", "css", "json", "tsx", "markdown", "markdown_inline", "gitignore", "python", "terraform", "hcl" }
+
     require("nvim-treesitter").setup({
-      ensure_installed = { "vim", "vimdoc", "lua", "java", "javascript", "typescript", "html", "css", "json", "tsx", "markdown", "markdown_inline", "gitignore", "python" },
       highlight = { enable = true },
-      autotag = {
-        enable = true
-      },
+      autotag = { enable = true },
     })
+
+    -- ensure_installed was removed in nvim-treesitter v1; install missing parsers explicitly
+    require("nvim-treesitter.install").install(parsers)
 
     -- nvim-treesitter-textobjects v2 requires manual keymap wiring;
     -- the old configs-based setup API no longer exists in treesitter v1
@@ -25,9 +27,18 @@ return {
       select = { lookahead = true, lookbehind = true },
     })
 
+    -- guard to silently skip textobject moves when no treesitter parser is available for the buffer
+    local function safe_move(fn, query, group)
+      return function()
+        local ok, parser = pcall(vim.treesitter.get_parser, 0)
+        if not ok or not parser then return end
+        fn(query, group)
+      end
+    end
+
     -- m / M: jump between methods
-    vim.keymap.set("n", "m", function() move.goto_next_start("@function.outer", "textobjects") end, { desc = "Next [M]ethod" })
-    vim.keymap.set("n", "M", function() move.goto_previous_start("@function.outer", "textobjects") end, { desc = "Previous [M]ethod" })
+    vim.keymap.set("n", "m", safe_move(move.goto_next_start, "@function.outer", "textobjects"), { desc = "Next [M]ethod" })
+    vim.keymap.set("n", "M", safe_move(move.goto_previous_start, "@function.outer", "textobjects"), { desc = "Previous [M]ethod" })
 
     -- am / im: select around/inside a method (works with d, y, c, v, =)
     vim.keymap.set({ "x", "o" }, "am", function() select.select_textobject("@function.outer", "textobjects") end, { desc = "[A]round [M]ethod" })
@@ -40,6 +51,32 @@ return {
     -- aa / ia: select around/inside an argument/parameter
     vim.keymap.set({ "x", "o" }, "aa", function() select.select_textobject("@parameter.outer", "textobjects") end, { desc = "[A]round [A]rgument" })
     vim.keymap.set({ "x", "o" }, "ia", function() select.select_textobject("@parameter.inner", "textobjects") end, { desc = "[I]nner [A]rgument" })
+
+    -- JSON: , / < jump between key-value pairs, scoped per-buffer to avoid
+    -- overriding comma and indent in other filetypes
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "json", "jsonc" },
+      callback = function()
+        local buf = { buffer = 0 }
+        vim.keymap.set("n", ",", safe_move(move.goto_next_start, "@pair.outer", "textobjects"), vim.tbl_extend("force", buf, { desc = "Next [P]air" }))
+        vim.keymap.set("n", "<", safe_move(move.goto_previous_start, "@pair.outer", "textobjects"), vim.tbl_extend("force", buf, { desc = "Previous [P]air" }))
+      end,
+    })
+
+    -- Terraform: m/M reuse @block.outer since terraform has no functions;
+    -- ab / ib: select around/inside a terraform block (resource, module, variable, data)
+    -- these only activate in terraform/hcl files via filetype autocmd to avoid
+    -- conflicting with the global m/M bindings
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "terraform", "hcl" },
+      callback = function()
+        local buf = { buffer = 0 }
+        vim.keymap.set("n", "m", safe_move(move.goto_next_start, "@block.outer", "textobjects"), vim.tbl_extend("force", buf, { desc = "Next [B]lock" }))
+        vim.keymap.set("n", "M", safe_move(move.goto_previous_start, "@block.outer", "textobjects"), vim.tbl_extend("force", buf, { desc = "Previous [B]lock" }))
+        vim.keymap.set({ "x", "o" }, "ab", function() select.select_textobject("@block.outer", "textobjects") end, vim.tbl_extend("force", buf, { desc = "[A]round [B]lock" }))
+        vim.keymap.set({ "x", "o" }, "ib", function() select.select_textobject("@block.inner", "textobjects") end, vim.tbl_extend("force", buf, { desc = "[I]nner [B]lock" }))
+      end,
+    })
 
     -- Configure ts-context-commentstring for native Neovim commenting
     require('ts_context_commentstring').setup({
---
+++ nvim/queries/json/textobjects.scm
@@ -0,0 +1,11 @@
+; extends
+
+(object) @function.outer
+
+(object
+  (_) @function.inner)
+
+(pair) @pair.outer
+
+(pair
+  value: (_) @pair.inner)
