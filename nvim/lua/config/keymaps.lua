4494ca28df7f378d45aef184c226f8881ae413d2
marcos <marcos@msegovia.dev>
2026-07-19 14:21:20 +0200

    feat(nvim): remap J/K to quarter-page scroll and H/L to line edges

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -43,11 +43,25 @@ vim.keymap.set("n", "<leader>wh", ":split<cr>", { desc = "[W]indow Split [H]oriz
 vim.keymap.set("v", "<", "<gv", { desc = "Indent left in visual mode" })
 vim.keymap.set("v", ">", ">gv", { desc = "Indent right in visual mode" })
 
--- Faster vertical scrolling (half page)
-vim.keymap.set("n", "J", "<C-d>", { desc = "Half page down" })
-vim.keymap.set("n", "K", "<C-u>", { desc = "Half page up" })
-vim.keymap.set("v", "J", "<C-d>", { desc = "Half page down" })
-vim.keymap.set("v", "K", "<C-u>", { desc = "Half page up" })
+-- Faster vertical scrolling (quarter page)
+-- Computed from the current window height so the jump adapts to any split size,
+-- while native <C-d>/<C-u> stay available for half-page jumps
+local function quarter_page(direction)
+  return function()
+    local quarter = math.max(1, math.floor(vim.api.nvim_win_get_height(0) / 4))
+    local count = vim.v.count > 0 and vim.v.count or 1
+    return (quarter * count) .. direction
+  end
+end
+
+vim.keymap.set("n", "J", quarter_page("j"), { expr = true, desc = "Quarter page down" })
+vim.keymap.set("n", "K", quarter_page("k"), { expr = true, desc = "Quarter page up" })
+vim.keymap.set("v", "J", quarter_page("j"), { expr = true, desc = "Quarter page down" })
+vim.keymap.set("v", "K", quarter_page("k"), { expr = true, desc = "Quarter page up" })
+
+-- Jump to line edges with H/L (vanilla H/L jump to top/bottom of screen)
+vim.keymap.set({ "n", "v" }, "H", "^", { desc = "Move to first non-blank of line" })
+vim.keymap.set({ "n", "v" }, "L", "$", { desc = "Move to end of line" })
 
 -- Comment keymaps (using native Neovim 0.10+ commenting)
 vim.keymap.set("n", "<leader>/", "gcc", { remap = true, desc = "Comment Line" })
